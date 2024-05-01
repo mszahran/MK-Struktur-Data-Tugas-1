bcef6f477a9fedc13488f6dd3f599c6a8ac9eff0
Zahran <muhammadzahran32@gmail.com>
2024-05-01 19:07:39 +0700

    push kode tugas-1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,57 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // No 01
+        int jumlahBaris;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // No 02
+        String kalimatBaru = "Deklarasi tipe data String";
+        System.out.println(kalimatBaru);
+
+        // No 03
+        int[] empatAngka = {7, 10, 20, 23};
+
+        // Menampilkan isi array empatAngka
+        System.out.println("Isi dari array empatAngka:");
+        for (int i = 0; i < empatAngka.length; i++) {
+            System.out.println(empatAngka[i]);
+        }
+
+        // No 04
+        String[][] alfabet = {
+                {"p", "s", "n"},
+                {"w", "l", "b"},
+                {"f", "r", "e"}
+        };
+
+        // Menampilkan isi array alfabet
+        System.out.println("Isi dari array alfabet:");
+        for (int i = 0; i < alfabet.length; i++) {
+            for (int j = 0; j < alfabet[i].length; j++) {
+                System.out.print(alfabet[i][j] + " ");
+            }
+            System.out.println(); // Mencetak baris baru setelah satu baris selesai ditampilkan
+        }
+
+        // No 05
+        // Membuat linked list dengan nama 'listAngka'
+        LinkedList<Integer> listAngka = new LinkedList<>();
+
+        // Menambahkan elemen ke linked list
+        listAngka.add(26);
+        listAngka.add(8);
+        listAngka.add(23);
+        listAngka.add(24);
+        listAngka.add(16);
+
+        // Menampilkan isi linked list
+        System.out.println("Isi dari linked list listAngka:");
+        for (Integer angka : listAngka) {
+            System.out.println(angka);
         }
     }
 }
